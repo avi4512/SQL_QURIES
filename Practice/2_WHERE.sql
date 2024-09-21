07cc90629ac66960ef49e7a9ad4db2f3d55ebf5d
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-09-21 16:18:21 +0530

    Create 2_WHERE.sql

---
+++ Practice/2_WHERE.sql
@@ -0,0 +1,15 @@
+#2.WHERE 
+#Which is used to filter the records
+#extract records based on the condition
+
+SELECT *
+FROM student
+WHERE id > 105;
+
+SELECT *
+FROM student
+WHERE per > 92;
+
+SELECT id,name
+FROM student
+WHERE per > 91;
