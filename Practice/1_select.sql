66a9a116395e09a84f88baf182ddbcc27f988141
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-09-21 16:13:54 +0530

    Create 1_select.sql

---
+++ Practice/1_select.sql
@@ -0,0 +1,25 @@
+#1.select statement
+  
+#Selecting all the columns in the table
+select * 
+from student;
+
+#selecting the particular columns
+select id,name
+from student;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
