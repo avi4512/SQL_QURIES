36bc89dbf957dbb4ae3165fdcbb84eeba915d36f
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-09-21 16:13:13 +0530

    Create 0_creation_table.sql

---
+++ Practice/0_creation_table.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE avi;
+
+show databases;
+use avi;
+
+create table student
+(id int,
+name char(10),
+branch char(10),
+per float);
+
+desc student;
+
+INSERT INTO student
+VALUES
+(101,'Avi','cse',90),
+(102,'Rama','ece',92),
+(103,'Hanuma','ece',91),
+(104,'Shiva','mech',94),
+(105,'vishnu','eee',90),
+(106,'Bramha','eee',90),
+(107,'Karna','cse',91),
+(108,'Arjuna','mech',88);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
