220c1aadc66013608c35c8ccb2bd8c82352dbc04
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-09-23 12:24:39 +0530

    Create 10_MIN()_MAX().sql

---
+++ Practice/10_MIN()_MAX().sql
@@ -0,0 +1,9 @@
+#10.MIN() MAX() functions
+#MIN --> return the smallest value in the selected column
+#MAX --> return the largest value in the selected column
+
+SELECT MIN(per) AS Least_percent
+FROM student;
+
+SELECT MAX(per) AS Largest_percentage
+FROM student;
