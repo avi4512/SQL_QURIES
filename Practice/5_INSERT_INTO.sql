051374477a5d70a47a9839374ba98bfd994d0e86
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-09-23 11:38:12 +0530

    Create 5_INSERT_INTO.sql

---
+++ Practice/5_INSERT_INTO.sql
@@ -0,0 +1,11 @@
+#5.INSERT INTO
+#-- It is used to insert new record into a table
+# INserting values to all the columns in the table
+  
+INSERT INTO student
+VALUES(109,'Dharama','cse',91);
+
+#to the specific columns
+  
+INSERT INTO student (id,name,per)
+VALUES (110,'Paandu',86);
