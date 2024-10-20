001d00fd1c55f99867b9d1da8114f8d78dd4718f
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-10-20 10:27:06 +0530

    Update 6_IS_NULL.sql

--- Practice/6_IS_NULL.sql
+++ Practice/6_IS_NULL.sql
@@ -1,14 +1,22 @@
-#6.IS NULL  ---- IS NOT NULL
-#A feild with a NULL value means a feild with no value
+--6.IS NULL  ---- IS NOT NULL
+--A feild with a NULL value means a feild with no value
 
-#IS NULL
-  
+--1.IS NULL
 SELECT *
 FROM student
 WHERE branch IS NULL;
 
-#IS NOT NULL
+--FIND THE COUNT OF NULL VALUES
+SELECT COUNT(*)
+FROM student
+WHERE branch IS NULL;
+
+--IS NOT NULL
 
 SELECT *
 FROM student
 WHERE branch IS NOT NULL;
+
+
+
+
